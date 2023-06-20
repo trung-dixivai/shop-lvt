3e07896f4dd5a2c5388d6d6f10f1f2463086b3b7
LeVanTrung <122528049+trung-dixivai@users.noreply.github.com>
2023-06-20 23:31:23 +0700

    q

---
+++ Home/JS/dangnhap.js
@@ -0,0 +1,38 @@
+function singup(e) {
+    event.preventDefault();
+    var name = document.getElementById("ten").value;
+    var ho = document.getElementById("ho").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var pass=document.getElementById("pass1").value;
+    if (ho === '' || email === '' || phone === '' || name === ''|| pass === '') {
+        alert("Vui lòng nhập đầy đủ thông tin!");
+    }
+else {
+    var user = {
+        name: name,
+        email: email,
+        ho: ho,
+        phone: phone,
+        pass:pass
+    }; 
+
+    var json = JSON.stringify(user);
+    localStorage.setItem(email, json);
+    alert("Đăng Kí Thành Công")
+}
+}
+function login(e) {
+    event.preventDefault();
+    var email1 = document.getElementById("hi").value;
+    var pass = document.getElementById("pass").value;
+    var user =localStorage.getItem(email1);
+    var data=JSON.parse(user)
+    if (email1 === '' || pass === '') {
+        alert("Vui lòng nhập đầy đủ thông tin!");
+    }
+    else {
+        alert("Vui Đăng Nhập Thành Công!");
+
+    }
+}
